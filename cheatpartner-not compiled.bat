0905b69ab1451f1ea3d3b29151d2f908de1e7653
ᵈᵉᵛHeXa <56086445+ImHeXa@users.noreply.github.com>
2021-08-12 13:52:50 +0200

    Add files via upload

---
+++ cheatpartner-not compiled.bat
@@ -0,0 +1,235 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJGmL9lAxJh5oQQWBLmT0D7YTiA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJGmL9lAxJh5obwyHPX+KB6cI5u3poe+fpy0=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title CCenter [By HeXa]
+color 09
+echo                  Warning!
+echo.
+echo  This program contains links to minecraft
+echo cheats. Many cracks can contain viruses, so
+echo you downloading everything from there at your
+echo own risk.
+echo.
+echo.
+echo Press Enter to continue...
+pause >nul
+goto menu
+
+:menu
+cls
+echo                  welcome!
+echo.
+echo TIP: For Cheat tests use eu.loyisa.cn
+echo.
+echo {1} - cheats
+echo {2} - configs
+echo {3} - texturepacks
+echo {4} - anticheats on each servers
+echo.
+echo {5} - launchers
+echo.
+echo {6} - credits
+echo.
+echo.
+set /p menu1=what do you want to do?: 
+if %menu1%==1 goto cheats
+if %menu1%==2 goto configs
+if %menu1%==3 goto texturepacks
+if %menu1%==4 goto acs
+if %menu1%==5 goto launchers
+if %menu1%==6 goto credits
+
+:cheats
+cls
+echo                   cheats
+echo.
+echo.
+echo You downloading all cheats at your own risk.
+echo Free cheats are unchecked and may contain
+echo any viruses.
+echo.
+echo {1} LiquidBounce B73 (idk probably 1.8)
+echo {2} LiquidBounce B72 (1.8)
+echo {3} Rise, Novoline, Tenacity, DortWare, ZeroDay, Azura (1.8-1.17)
+echo {4} azura (1.8)
+echo {5} Asyncware Client (1.8)
+echo {6} Wurst Client (1.8)
+echo {7} Wurst Client (1.17)
+echo {8} Envy (idk)
+echo {9} Lite (probably 1.6)
+echo {10} Koid Ghost Client (VERY RISKY)
+echo {11} Remix (1.8)
+echo.
+echo.
+set /p menu2=select cheat: 
+if %menu2%==1 start https://dl.ccbluex.net/download/AjcnSZCJLt && goto menu
+if %menu2%==2 start https://dl.ccbluex.net/download/9EIpazmhSL && goto menu
+if %menu2%==3 start https://intent.store/ && goto menu
+if %menu2%==4 start https://azura.best/ && goto menu
+if %menu2%==5 start https://asyncware.wtf/ && goto menu
+if %menu2%==6 start https://www.wurstclient.net/updates/wurst-6-35/ && goto menu
+if %menu2%==7 start https://www.wurstclient.net/updates/wurst-7-16/ && goto menu
+if %menu2%==8 start https://www.mediafire.com/file/acih0clwkuwvvao/Envy.rar/file && goto menu
+if %menu2%==9 start https://www.mediafire.com/file/pz53tt45wdmpv86/Lite_1.6.zip/file && goto menu
+if %menu2%==10 start https://www.mediafire.com/file/z0ru7n131vwtzjn/Koid.exe/file && goto menu
+if %menu2%==11 start https://remix-client.selly.store/product/139f6eeb && goto menu
+
+:configs
+cls
+echo                   configs
+echo.
+echo.
+echo None!
+echo Press Enter to go back to menu.
+pause >nul
+goto menu
+
+:texturepacks
+cls
+echo                 texturepacks
+echo.
+echo.
+echo {1} RkyFault
+echo {2} 200 Subs Packs Special by Ravel
+echo {3} FaithfulVenom
+echo {4} Exhibition Pack
+echo {5} SmoothAnimations
+echo {6} BetterVanillaBuilding
+echo {7} YegsPack (1.17)
+echo {8} DefaultDarkMode
+echo {9} Cursed
+echo {10} AnimatedChroma
+echo {11} Green PVP Pack
+echo {12} UBER Pack (1.8)
+echo {13} UBER Pack (1.16)
+echo.
+set /p menu3=select txt: 
+if %menu3%==1 start http://www.mediafire.com/file/f41ak2n19kqep1t/%2521_%25C2%25A7bRKYfault%25C2%25A73%255B16x%255D.zip/file && goto menu
+if %menu3%==2 start https://www.mediafire.com/file/2p1zjeahqo35ugi/pack_folder18.zip/file && goto menu
+if %menu3%==3 start https://antvenom.com/files && goto menu
+if %menu3%==4 start https://botein.workupload.com/download/sB5GR2WQwFr && goto menu
+if %menu3%==5 start https://www.curseforge.com/minecraft/texture-packs/fresh-animations && goto menu
+if %menu3%==6 start https://www.curseforge.com/minecraft/texture-packs/bettervanillabuilding && goto menu
+if %menu3%==7 start https://www.dropbox.com/s/w3t7r4onf761b9x/YegsPack%201.17.zip?dl=0 && goto menu
+if %menu3%==8 start https://resourcepack.net/default-dark-mode-resource-pack/ && goto menu
+if %menu3%==9 start https://www.curseforge.com/minecraft/texture-packs/cursed-pack && goto menu
+if %menu3%==10 start https://www.mediafire.com/file/t9zi5ehhpf2d6la/AnimatedChroma%25231.zip/file && goto menu
+if %menu3%==11 start https://www.mediafire.com/file/bg6vi4xb91tdsz2/%2521_%25C2%25A7aToxic%25C2%25A72Green_%25C2%25A78%255B%25C2%25A7f32%25C2%25A7ax%25C2%25A78%255D.zip/file && goto menu
+if %menu3%==12 start https://www.mediafire.com/file/tjm2bej6q0gnkb9/!+++++%C2%A73%C2%A7lTXT+%C2%A78%C2%A7lGILDIA+UBER.rar/file && goto menu
+if %menu3%==13 start https://www.mediafire.com/file/v43gq1v4trz1diw/!+++++%C2%A73%C2%A7lTXT+%C2%A78%C2%A7lGILDIA+UBER+1+16.rar/file && goto menu
+
+:acs
+cls
+echo                    anticheats
+echo.
+echo.
+echo Dragonsurvival.eu - Verus+NoCheatPlus (NCP)
+echo Kokscraft.pl - Vulcan 
+echo Craftplay.pl - Verus+(Matrix Probably)
+echo DerpMC.pl - Advanced AntiCheat (AAC)
+echo hypixel.net - WatchDog (Modified NoCheatPlus)
+echo Cubecraft.net - Sentinel 
+echo minemen.club - AntiGamingChair (AGC)
+echo Ghostly - Ghost
+echo play.luckynetwork.net - Verus
+echo play.noobcraft.es - Vanilla Kick 
+echo mc.minemora.net - NoCheatPlus (NCP)
+echo mush.com.br - idk (Silent bad AC)
+echo Mineplay.pl - Spartan
+echo Acmc.pl - Old Verus
+echo CraftPlay.pl - WatchCat (after 21.08.2021)
+echo VictoryCraft - AAC v5.0.40
+echo.
+echo Click enter to go back to menu.
+pause >nul
+goto menu
+
+:launchers
+cls
+echo                launchers and packs
+echo.
+echo.
+echo {1} Lunar Client
+echo {2} Badlion Client
+echo {3} OnlyWin Client
+echo {4} Crystal launcher
+echo {5} TLauncher
+echo.
+echo {6} Original minecraft launcher
+echo.
+echo {7} Fabric
+echo {8} Forge
+echo {9} OptiFine
+echo.
+set /p menu5=select pack: 
+if %menu5%==1 start https://www.lunarclient.com/ && goto menu
+if %menu5%==2 start https://www.badlion.net/ && goto menu
+if %menu5%==3 start https://discord.gg/rkB3RMurdJ && goto menu
+if %menu5%==4 start https://crystal-launcher.pl/ && goto menu
+if %menu5%==5 start https://tlauncher.org/en/ && goto menu
+if %menu5%==6 start https://www.minecraft.net/pl-pl && goto menu
+if %menu5%==7 start https://fabricmc.net/ && goto menu
+if %menu5%==8 start https://files.minecraftforge.net/net/minecraftforge/forge/ && goto menu
+if %menu5%==9 start https://optifine.net/home && goto menu
+
+:credits
+cls
+echo Author: HeXa
+echo Copyrights:
+echo.
+echo This Program: © HeXa
+echo LiquidBounce © CCBlueX 2015-2021
+echo Intent.Store ©
+echo azura ©
+echo Asyncware © nquantum 2021
+echo Wurst © Alexander01998 2021
+echo Mediafire ©
+echo selly.store ©
+echo remix client ©
+echo Lunar Client ©
+echo Badlion Client ©
+echo OnlyWin Client © litedark 2021
+echo Crystal Launcher ©
+echo TLauncher ©
+echo Forge ©
+echo Fabric ©
+echo Minecraft © Mojang 2021
+echo.
+echo All credits are here, if i forget someone, DM me on discord or mail me:
+echo hexaofficial@yahoo.com
+echo HeXa#2137
+echo.
+echo Press Enter to go back to menu.
+pause >nul
+goto menu
